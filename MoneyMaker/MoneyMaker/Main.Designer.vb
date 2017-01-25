d50996fa5d213552f755ded1214f448f3f8a3035
flareboy127 <flareboy127@users.noreply.github.com>
2017-01-25 14:18:59 -0500

    Added MoneyMaker

---
+++ MoneyMaker/MoneyMaker/Main.Designer.vb
@@ -0,0 +1,399 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.Spend = New System.Windows.Forms.TabControl()
+        Me.Upgrades = New System.Windows.Forms.TabPage()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.BDo = New System.Windows.Forms.Button()
+        Me.BTaxC = New System.Windows.Forms.Button()
+        Me.Workers = New System.Windows.Forms.TabPage()
+        Me.BWork = New System.Windows.Forms.Button()
+        Me.Construction = New System.Windows.Forms.TabPage()
+        Me.Research = New System.Windows.Forms.TabPage()
+        Me.BCityHall = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Money = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Ore = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SecondCount = New System.Windows.Forms.Timer(Me.components)
+        Me.ResetTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.BCOre = New System.Windows.Forms.Button()
+        Me.Spend.SuspendLayout()
+        Me.Upgrades.SuspendLayout()
+        Me.Workers.SuspendLayout()
+        Me.Construction.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(941, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "Main Menu"
+        '
+        'Spend
+        '
+        Me.Spend.Controls.Add(Me.Upgrades)
+        Me.Spend.Controls.Add(Me.Workers)
+        Me.Spend.Controls.Add(Me.Construction)
+        Me.Spend.Controls.Add(Me.Research)
+        Me.Spend.Location = New System.Drawing.Point(696, 80)
+        Me.Spend.Name = "Spend"
+        Me.Spend.SelectedIndex = 0
+        Me.Spend.Size = New System.Drawing.Size(245, 503)
+        Me.Spend.TabIndex = 1
+        '
+        'Upgrades
+        '
+        Me.Upgrades.Controls.Add(Me.Button13)
+        Me.Upgrades.Controls.Add(Me.Button9)
+        Me.Upgrades.Controls.Add(Me.Button14)
+        Me.Upgrades.Controls.Add(Me.Button10)
+        Me.Upgrades.Controls.Add(Me.Button15)
+        Me.Upgrades.Controls.Add(Me.Button11)
+        Me.Upgrades.Controls.Add(Me.Button16)
+        Me.Upgrades.Controls.Add(Me.Button12)
+        Me.Upgrades.Controls.Add(Me.Button5)
+        Me.Upgrades.Controls.Add(Me.Button6)
+        Me.Upgrades.Controls.Add(Me.Button7)
+        Me.Upgrades.Controls.Add(Me.Button8)
+        Me.Upgrades.Controls.Add(Me.Button4)
+        Me.Upgrades.Controls.Add(Me.Button3)
+        Me.Upgrades.Controls.Add(Me.BDo)
+        Me.Upgrades.Controls.Add(Me.BTaxC)
+        Me.Upgrades.Location = New System.Drawing.Point(4, 22)
+        Me.Upgrades.Name = "Upgrades"
+        Me.Upgrades.Padding = New System.Windows.Forms.Padding(3)
+        Me.Upgrades.Size = New System.Drawing.Size(237, 477)
+        Me.Upgrades.TabIndex = 0
+        Me.Upgrades.Text = "Upgrades"
+        Me.Upgrades.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(6, 442)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(223, 23)
+        Me.Button13.TabIndex = 8
+        Me.Button13.Text = "Button13"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(6, 326)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(223, 23)
+        Me.Button9.TabIndex = 11
+        Me.Button9.Text = "Button9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(6, 413)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(223, 23)
+        Me.Button14.TabIndex = 7
+        Me.Button14.Text = "Button14"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(6, 297)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(223, 23)
+        Me.Button10.TabIndex = 10
+        Me.Button10.Text = "Button10"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(6, 384)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(223, 23)
+        Me.Button15.TabIndex = 6
+        Me.Button15.Text = "Button15"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(6, 268)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(223, 23)
+        Me.Button11.TabIndex = 9
+        Me.Button11.Text = "Button11"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(6, 355)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(223, 23)
+        Me.Button16.TabIndex = 5
+        Me.Button16.Text = "Button16"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(6, 239)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(223, 23)
+        Me.Button12.TabIndex = 8
+        Me.Button12.Text = "Button12"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(6, 210)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(223, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(6, 181)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(223, 23)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(6, 152)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(223, 23)
+        Me.Button7.TabIndex = 5
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(6, 123)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(223, 23)
+        Me.Button8.TabIndex = 4
+        Me.Button8.Text = "Button8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(6, 94)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(223, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(6, 65)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(223, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'BDo
+        '
+        Me.BDo.Location = New System.Drawing.Point(6, 36)
+        Me.BDo.Name = "BDo"
+        Me.BDo.Size = New System.Drawing.Size(223, 23)
+        Me.BDo.TabIndex = 1
+        Me.BDo.Text = "Donations ($5/sec):"
+        Me.BDo.UseVisualStyleBackColor = True
+        '
+        'BTaxC
+        '
+        Me.BTaxC.Location = New System.Drawing.Point(6, 7)
+        Me.BTaxC.Name = "BTaxC"
+        Me.BTaxC.Size = New System.Drawing.Size(223, 23)
+        Me.BTaxC.TabIndex = 0
+        Me.BTaxC.Text = "Tax Collector ($1/sec):"
+        Me.BTaxC.UseVisualStyleBackColor = True
+        '
+        'Workers
+        '
+        Me.Workers.Controls.Add(Me.BWork)
+        Me.Workers.Location = New System.Drawing.Point(4, 22)
+        Me.Workers.Name = "Workers"
+        Me.Workers.Size = New System.Drawing.Size(237, 477)
+        Me.Workers.TabIndex = 3
+        Me.Workers.Text = "Workers"
+        Me.Workers.UseVisualStyleBackColor = True
+        '
+        'BWork
+        '
+        Me.BWork.Location = New System.Drawing.Point(3, 3)
+        Me.BWork.Name = "BWork"
+        Me.BWork.Size = New System.Drawing.Size(231, 39)
+        Me.BWork.TabIndex = 0
+        Me.BWork.Text = "Construction Worker:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Unlocks Construction"
+        Me.BWork.UseVisualStyleBackColor = True
+        '
+        'Construction
+        '
+        Me.Construction.Controls.Add(Me.BCOre)
+        Me.Construction.Location = New System.Drawing.Point(4, 22)
+        Me.Construction.Name = "Construction"
+        Me.Construction.Size = New System.Drawing.Size(237, 477)
+        Me.Construction.TabIndex = 2
+        Me.Construction.Text = "Construction"
+        Me.Construction.UseVisualStyleBackColor = True
+        '
+        'Research
+        '
+        Me.Research.Location = New System.Drawing.Point(4, 22)
+        Me.Research.Name = "Research"
+        Me.Research.Padding = New System.Windows.Forms.Padding(3)
+        Me.Research.Size = New System.Drawing.Size(237, 477)
+        Me.Research.TabIndex = 1
+        Me.Research.Text = "Research"
+        Me.Research.UseVisualStyleBackColor = True
+        '
+        'BCityHall
+        '
+        Me.BCityHall.Location = New System.Drawing.Point(283, 247)
+        Me.BCityHall.Name = "BCityHall"
+        Me.BCityHall.Size = New System.Drawing.Size(103, 87)
+        Me.BCityHall.TabIndex = 3
+        Me.BCityHall.Text = "City Hall"
+        Me.BCityHall.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Money, Me.Ore, Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 586)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(941, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Money
+        '
+        Me.Money.Name = "Money"
+        Me.Money.Size = New System.Drawing.Size(19, 17)
+        Me.Money.Text = "$0"
+        '
+        'Ore
+        '
+        Me.Ore.Name = "Ore"
+        Me.Ore.Size = New System.Drawing.Size(38, 17)
+        Me.Ore.Text = "Ore: 0"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(44, 17)
+        Me.ToolStripStatusLabel1.Text = "Steel: 0"
+        '
+        'SecondCount
+        '
+        Me.SecondCount.Interval = 1000
+        '
+        'ResetTimer
+        '
+        '
+        'BCOre
+        '
+        Me.BCOre.Location = New System.Drawing.Point(3, 3)
+        Me.BCOre.Name = "BCOre"
+        Me.BCOre.Size = New System.Drawing.Size(231, 43)
+        Me.BCOre.TabIndex = 0
+        Me.BCOre.Text = "Ore Mine:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(+ 0.2 ore/second)"
+        Me.BCOre.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(941, 608)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.BCityHall)
+        Me.Controls.Add(Me.Spend)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Main"
+        Me.Text = "Money Maker"
+        Me.Spend.ResumeLayout(False)
+        Me.Upgrades.ResumeLayout(False)
+        Me.Workers.ResumeLayout(False)
+        Me.Construction.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents Spend As System.Windows.Forms.TabControl
+    Friend WithEvents Upgrades As System.Windows.Forms.TabPage
+    Friend WithEvents Research As System.Windows.Forms.TabPage
+    Friend WithEvents Construction As System.Windows.Forms.TabPage
+    Friend WithEvents BCityHall As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents BDo As System.Windows.Forms.Button
+    Friend WithEvents BTaxC As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents Money As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Ore As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents SecondCount As System.Windows.Forms.Timer
+    Friend WithEvents ResetTimer As System.Windows.Forms.Timer
+    Friend WithEvents Workers As System.Windows.Forms.TabPage
+    Friend WithEvents BWork As System.Windows.Forms.Button
+    Friend WithEvents BCOre As System.Windows.Forms.Button
+
+End Class
---
+++ MoneyMaker/MoneyMaker/Main.vb
@@ -0,0 +1,145 @@
+﻿Public Class Main
+#Region "Vars"
+    'Resources
+    Public totalMoney As Double
+    Public totalOre As Double
+    Public totalSteel As Double
+    'Earn Per Second
+    Public TaxCollectors As Integer
+    Public TaxCollectorsC As Integer
+    Public Donations As Integer
+    Public DonationsC As Integer
+    'Workers
+    Public cWorker As Integer
+    Public cWorkerC As Integer
+    'Construction
+    Public cOreM As Integer
+    Public cOreMC As Integer
+#End Region
+#Region "Setup"
+    Private Sub BCityHall_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BCityHall.Click
+        totalMoney += 1
+        Money.Text = totalMoney.ToString("$0.00")
+    End Sub
+
+    Private Sub ResetTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ResetTimer.Tick
+        'Button on/off
+        'Upgrades
+        If totalMoney >= TaxCollectorsC Then
+            BTaxC.Enabled = True
+        Else
+            BTaxC.Enabled = False
+        End If
+
+        If totalMoney >= DonationsC Then
+            BDo.Enabled = True
+        Else
+            BDo.Enabled = False
+        End If
+
+
+        'Workers
+        If totalMoney >= cWorkerC Then
+            BWork.Enabled = True
+        Else
+            BWork.Enabled = False
+        End If
+
+
+        'Construction
+        If totalMoney >= cOreMC And cWorker > 0 Then
+            BCOre.Enabled = True
+        Else
+            BCOre.Enabled = False
+        End If
+    End Sub
+
+    Private Sub SecondCount_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SecondCount.Tick
+        'Money per second
+        totalMoney += TaxCollectors * 1
+        totalMoney += Donations * 5
+
+        Money.Text = totalMoney.ToString("$0.00")
+    End Sub
+
+    Private Sub Main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        SecondCount.Start()
+        ResetTimer.Start()
+        'Upgrades
+        TaxCollectorsC = 10 : BTaxC.Text = "Tax Collector ($1/sec): " & TaxCollectorsC.ToString("$0")
+        DonationsC = 50 : BDo.Text = "Donations ($5/sec): " & DonationsC.ToString("$0")
+        'Workers
+        cWorkerC = 4000 : BWork.Text = "Construction Worker: " & cWorkerC.ToString("$0") & vbCrLf & "(Unlocks Construction)"
+        'Construction
+        cOreMC = 2000 : BCOre.Text = "Ore Mine: " & cOreMC.ToString("$0") & vbCrLf & "(+ 0.2 ore/second)"
+    End Sub
+
+    'Remove money to buy : output new balance to label
+    'Add a collector
+    'Increase cost to buy : output new cost to label
+#Region "Upgrades Setup"
+    Private Sub BTaxC_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTaxC.Click
+        totalMoney -= TaxCollectorsC : Money.Text = totalMoney.ToString("$0.00")
+        TaxCollectors += 1
+        TaxCollectorsC = TaxCollectorsC * 1.25 : BTaxC.Text = "Tax Collector ($1/sec): " & TaxCollectorsC.ToString("$0")
+    End Sub
+
+    Private Sub BDo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BDo.Click
+        totalMoney -= DonationsC : Money.Text = totalMoney.ToString("$0.00")
+        Donations += 1
+        DonationsC = DonationsC * 1.5 : BDo.Text = "Donations ($5/sec): " & DonationsC.ToString("$0")
+    End Sub
+
+#End Region
+#Region "Workers Setup"
+    Private Sub BWork_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BWork.Click
+        totalMoney -= cWorkerC : Money.Text = totalMoney.ToString("$0.00")
+        cWorker += 1
+        cWorkerC = cWorkerC * 3 : BWork.Text = "Construction Worker: " & cWorkerC.ToString("$0") & vbCrLf & "(+ 0.5/sec Construction Speed)"
+    End Sub
+#End Region
+#Region "Construction Setup"
+    Private Sub BCOre_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BCOre.Click
+        totalMoney -= cOreMC : Money.Text = totalMoney.ToString("$0.00")
+        cOreM += 1
+        cOreMC = cOreMC * 2 : BCOre.Text = "Ore Mine: " & cOreMC.ToString("$0") & vbCrLf & "(+ 0.2 ore/second)"
+    End Sub
+#End Region
+#End Region
+
+
+
+End Class
+
+'<--------------------- NOTES ------------------------------>
+'   -> Constuction workers make buildings
+'   ->
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'<--------------------- IDEAS ------------------------------>
+'   -> Workers that cost an ammount of upgrades instead of money
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
+'
